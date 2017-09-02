a6640b15d61bd1cfbf9cba3ecb46a02eb41c1c59
Wizard <wizard@Wizards-MacBook-Air.local>
2017-09-02 13:36:49 -1000

    Use our own fake API server in express

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,9 +1,10 @@
 'use static';
 
-var config = require('config.json');
+var config = require('./config.json');
 
-exports.handler = function (event, context, callback) {
-  callback(null, {
+exports.handler = function (event, context) {
+  console.log(1, event);
+  context.succeed({
     statusCode: '200',
     body: config
   });
