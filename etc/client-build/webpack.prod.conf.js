0434dccd690dd04aab3326e66b063830f9d21e62
Chris <cshfang@gmail.com>
2017-09-04 13:56:56 -1000

    Added 404.html. Webpack build now copies 404.html to static. Moved built client file from dist to dist/client

--- etc/client-build/webpack.prod.conf.js
+++ etc/client-build/webpack.prod.conf.js
@@ -88,9 +88,13 @@ var webpackConfig = merge(baseWebpackConfig, {
     // copy custom static assets
     new CopyWebpackPlugin([
       {
-        from: path.resolve(__dirname, '../static'),
+        from: path.resolve(__dirname, '../../static'),
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
+      },
+      {
+        from: path.resolve(__dirname, '../../404.html'),
+        to: config.build.assetsSubDirectory
       }
     ])
   ]
