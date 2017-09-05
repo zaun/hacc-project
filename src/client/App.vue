e11b320adde131726b5060d1ad91906c6d7bc17b
Chris <cshfang@gmail.com>
2017-09-04 16:51:11 -1000

    Added Site Scenario UI elements. Added google font

--- src/client/App.vue
+++ src/client/App.vue
@@ -10,4 +10,12 @@ export default {
 </script>
 
 <style lang="stylus">
+html
+body
+  width 100%
+  height 100%
+  font-family 'Source Sans Pro', sans-serif
+
+body
+  background-color whitesmoke
 </style>
--- src/client/views/Home.vue
+++ src/client/views/Home.vue
@@ -8,6 +8,42 @@
             .column.is-half-mobile.is-one-third-tablet.is-one-quarter-desktop
               logo(hero)
             .column
+    section.section
+      .container
+        h1.title.is-2.has-text-grey.has-text-centered Site Scenario
+        .box.is-hidden-tablet
+          .columns(v-for='siteScenario in siteScenarios')
+            .column
+              .field
+                label.label.has-text-centered {{ siteScenario.label }}
+                .control
+                  a.button.is-primary.is-fullwidth(
+                    @click='selectScenario(siteScenario, 0)'
+                    :class='scenarioButtonClass(siteScenario, 0)')
+                    span {{ siteScenario[0].name }}
+                .control
+                  a.button.is-primary.is-fullwidth(
+                    @click='selectScenario(siteScenario, 1)'
+                    :class='scenarioButtonClass(siteScenario, 1)')
+                    span {{ siteScenario[1].name }}
+        .box.is-hidden-mobile
+          .columns(v-for='siteScenario in siteScenarios')
+            .column
+              .field.is-horizontal
+                .field-label
+                  label.label {{ siteScenario.label }}
+                .field-body
+                  .control
+                    a.button.is-primary(
+                      @click='selectScenario(siteScenario, 0)'
+                      :class='scenarioButtonClass(siteScenario, 0)')
+                      span {{ siteScenario[0].name }}
+                  .control
+                    a.button.is-primary(
+                      @click='selectScenario(siteScenario, 1)'
+                      :class='scenarioButtonClass(siteScenario, 1)')
+                      span {{ siteScenario[1].name }}
+
 </template>
 
 <script>
@@ -17,15 +53,75 @@ export default {
   name: 'home',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      siteScenarios: [
+        {
+          label: 'Land use',
+          active: 0,
+          0: { name: 'Unrestricted' },
+          1: { name: 'Commercial/Industrial only' }
+        },
+        {
+          label: 'Groundwater utility',
+          active: 0,
+          0: { name: 'Drinking water resource' },
+          1: { name: 'Nondrinking water resource' }
+        },
+        {
+          label: 'Distance to nearest surface water body',
+          active: 0,
+          0: { name: 'Under 150 meters' },
+          1: { name: '150 meters or more' }
+        }
+      ]
     };
+  },
+  methods: {
+    selectScenario (siteScenario, index) {
+      siteScenario.active = index;
+    },
+    scenarioButtonClass (siteScenario, index) {
+      return {
+        'is-active': siteScenario.active === index,
+        'is-outlined': siteScenario.active !== index
+      };
+    }
   },
   components: { Logo }
 };
 </script>
 
 <style lang="stylus" scoped>
-#home .logo
+.logo
   width 100%
   max-width 400px
+
+h1.title
+  margin-bottom 0
+  font-weight 300
+
+.is-hidden-tablet .field
+  .control .button
+    border-top-left-radius 10px
+    border-top-right-radius 10px
+    border-bottom-left-radius 0
+    border-bottom-right-radius 0
+  .control:last-child .button
+    border-top-left-radius 0
+    border-top-right-radius 0
+    border-bottom-left-radius 10px
+    border-bottom-right-radius 10px
+
+.is-hidden-mobile .field
+  .field-label
+    flex-grow 3.5
+  .control:first-child .button
+    border-top-left-radius 20px
+    border-top-right-radius 0
+    border-bottom-left-radius 20px
+    border-bottom-right-radius 0
+  .control:last-child .button
+    border-top-left-radius 0
+    border-top-right-radius 20px
+    border-bottom-left-radius 0
+    border-bottom-right-radius 20px
 </style>
