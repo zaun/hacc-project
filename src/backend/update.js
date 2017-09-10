fd9435ca2babcd2f1c0a701965834dcc57746531
Justin Zaun <justin.zaun@gmail.com>
2017-09-09 15:19:39 -1000

    Import all D tables

--- src/backend/update.js
+++ src/backend/update.js
@@ -71,6 +71,14 @@ var getDataRows = function (data, cb) {
     if (row[0] === 'Notes:' || row[1] === 'Notes:') {
       return false;
     }
+
+    // OTHER
+    if (row[0] === 'Source (unless otherwise noted):') {
+      return false;
+    }
+    if (row[0] === 'References:') {
+      return false;
+    }
     rows.push(row);
   });
 
@@ -374,6 +382,272 @@ exports.handler = function (event, context) {
           commercialLowest: cleanValue(row[8]),
           commercialCarcinogenicEffects: cleanValue(row[9]),
           commercialNoncarcinogenicEffects: cleanValue(row[10])
+        };
+      }, nextStep);
+    },
+
+    /*
+     * Table D-1a
+     */
+    function (nextStep) {
+      importData(data, 'tableD1a', 'TABLE D-1a. GROUNDWATER ACTION LEVELS', 5, function (row) {
+        return {
+          chemical: cleanValue(row[0]),
+          finalGroundwaterActionLevel: cleanValue(row[1]),
+          basis: cleanValue(row[2]),
+          grossContamination: cleanValue(row[3]),
+          drinkingWaterToxicity: cleanValue(row[4]),
+          vaporIntrusion: cleanValue(row[5]),
+          aquaticHabitatImpacts: cleanValue(row[6])
+        };
+      }, nextStep);
+    },
+
+    /*
+     * Table D-1b
+     */
+    function (nextStep) {
+      importData(data, 'tableD1b', 'TABLE D-1b. GROUNDWATER ACTION LEVELS', 5, function (row) {
+        return {
+          chemical: cleanValue(row[0]),
+          finalGroundwaterActionLevel: cleanValue(row[1]),
+          basis: cleanValue(row[2]),
+          grossContamination: cleanValue(row[3]),
+          drinkingWaterToxicity: cleanValue(row[4]),
+          vaporIntrusion: cleanValue(row[5]),
+          aquaticHabitatImpacts: cleanValue(row[6])
+        };
+      }, nextStep);
+    },
+
+    /*
+     * Table D-1c
+     */
+    function (nextStep) {
+      importData(data, 'tableD1c', 'TABLE D-1c. GROUNDWATER ACTION LEVELS', 5, function (row) {
+        return {
+          chemical: cleanValue(row[0]),
+          finalGroundwaterActionLevel: cleanValue(row[1]),
+          basis: cleanValue(row[2]),
+          grossContamination: cleanValue(row[3]),
+          vaporIntrusion: cleanValue(row[4]),
+          aquaticHabitatImpacts: cleanValue(row[5])
+        };
+      }, nextStep);
+    },
+
+    /*
+     * Table D-1d
+     */
+    function (nextStep) {
+      importData(data, 'tableD1d', 'TABLE D-1d. GROUNDWATER ACTION LEVELS', 5, function (row) {
+        return {
+          chemical: cleanValue(row[0]),
+          finalGroundwaterActionLevel: cleanValue(row[1]),
+          basis: cleanValue(row[2]),
+          grossContamination: cleanValue(row[3]),
+          vaporIntrusion: cleanValue(row[4]),
+          aquaticHabitatImpacts: cleanValue(row[5])
+        };
+      }, nextStep);
+    },
+
+    /*
+     * Table D-2a
+     */
+    function (nextStep) {
+      importData(data, 'tableD2a', 'TABLE D-2a. SURFACE WATER ACTION LEVELS', 5, function (row) {
+        return {
+          chemical: cleanValue(row[0]),
+          finalGroundwaterActionLevel: cleanValue(row[1]),
+          basis: cleanValue(row[2]),
+          grossContamination: cleanValue(row[3]),
+          drinkingWaterToxicity: cleanValue(row[4]),
+          vaporIntrusion: cleanValue(row[5]),
+          aquaticHabitatImpacts: cleanValue(row[6])
+        };
+      }, nextStep);
+    },
+
+    /*
+     * Table D-2b
+     */
+    function (nextStep) {
+      importData(data, 'tableD2b', 'TABLE D-2b. SURFACE WATER ACTION LEVELS', 5, function (row) {
+        return {
+          chemical: cleanValue(row[0]),
+          finalGroundwaterActionLevel: cleanValue(row[1]),
+          basis: cleanValue(row[2]),
+          grossContamination: cleanValue(row[3]),
+          vaporIntrusion: cleanValue(row[4]),
+          aquaticHabitatImpacts: cleanValue(row[5])
+        };
+      }, nextStep);
+    },
+
+    /*
+     * Table D-2c
+     */
+    function (nextStep) {
+      importData(data, 'tableD2c', 'TABLE D-2c. SURFACE WATER ACTION LEVELS', 5, function (row) {
+        return {
+          chemical: cleanValue(row[0]),
+          finalGroundwaterActionLevel: cleanValue(row[1]),
+          basis: cleanValue(row[2]),
+          grossContamination: cleanValue(row[3]),
+          vaporIntrusion: cleanValue(row[4]),
+          aquaticHabitatImpacts: cleanValue(row[5])
+        };
+      }, nextStep);
+    },
+
+    /*
+     * Table D-3a
+     */
+    function (nextStep) {
+      importData(data, 'tableD3a', 'TABLE D-3a. FINAL DRINKING WATER ACTION LEVELS FOR HUMAN TOXICITY', 5, function (row) {
+        return {
+          chemical: cleanValue(row[0]),
+          finalActionLevel: cleanValue(row[1]),
+          basis: cleanValue(row[2]),
+          grossContamination: cleanValue(row[3]),
+          HDOHPrimaryMCL: cleanValue(row[4]),
+          otherCriteria: cleanValue(row[5]),
+          reference: cleanValue(row[6]),
+          riskBasedAcctionLevel: cleanValue(row[7]),
+          basisB: cleanValue(row[8])
+        };
+      }, nextStep);
+    },
+
+    /*
+     * Table D-3b
+     */
+    function (nextStep) {
+      importData(data, 'tableD3b', 'TABLE D-3b. RISK-BASED ACTION LEVELS FOR TAPWATER', 5, function (row) {
+        return {
+          chemical: cleanValue(row[0]),
+          lowestTapwaterGoal: cleanValue(row[1]),
+          basis: cleanValue(row[2]),
+          carcinogenicEffects: cleanValue(row[3]),
+          mutagenicEffects: cleanValue(row[4]),
+          noncancerEffects: cleanValue(row[5])
+        };
+      }, nextStep);
+    },
+
+    /*
+     * Table D-4a
+     */
+    function (nextStep) {
+      importData(data, 'tableD4a', 'TABLE D-4a.  SUMMARY OF AQUATIC HABITAT GOALS', 4, function (row) {
+        return {
+          chemical: cleanValue(row[0]),
+          estuarineChronicAquaticToxicity: cleanValue(row[1]),
+          estuarineAcuteAquaticToxicity: cleanValue(row[2]),
+          freshwaterChronicAquaticToxicity: cleanValue(row[3]),
+          freshwaterAcuteAquaticToxicity: cleanValue(row[4]),
+          marineChronicAquaticToxicity: cleanValue(row[5]),
+          marineAcuteAquaticToxicity: cleanValue(row[6])
+        };
+      }, nextStep);
+    },
+
+    /*
+     * Table D-4b
+     */
+    function (nextStep) {
+      importData(data, 'tableD4b', 'TABLE D-4b. SUMMARY OF SELECTED CHRONIC AQUATIC HABITAT GOALS', 4, function (row) {
+        return {
+          chemical: cleanValue(row[0]),
+          estuarineAquaticHabitatGoal: cleanValue(row[1]),
+          estuarineBasis: cleanValue(row[2]),
+          freshwaterAquaticHabitatGoal: cleanValue(row[3]),
+          freshwaterBasis: cleanValue(row[4]),
+          marineAquaticHabitatGoal: cleanValue(row[5]),
+          marineBasis: cleanValue(row[6])
+        };
+      }, nextStep);
+    },
+
+    /*
+     * Table D-4c
+     */
+    function (nextStep) {
+      importData(data, 'tableD4c', 'TABLE D-4c. SUMMARY OF SELECTED ACUTE AQUATIC HABITAT GOALS', 4, function (row) {
+        return {
+          chemical: cleanValue(row[0]),
+          estuarineAquaticHabitatGoal: cleanValue(row[1]),
+          estuarineBasis: cleanValue(row[2]),
+          freshwaterAquaticHabitatGoal: cleanValue(row[3]),
+          freshwaterBasis: cleanValue(row[4]),
+          marineAquaticHabitatGoal: cleanValue(row[5]),
+          marineBasis: cleanValue(row[6])
+        };
+      }, nextStep);
+    },
+
+    /*
+     * Table D-4d
+     */
+    function (nextStep) {
+      importData(data, 'tableD4d', 'TABLE D-4d. SUMMARY OF HAWAI\'I CHRONIC AND ACUTE', 4, function (row) {
+        return {
+          chemical: cleanValue(row[0]),
+          freshwaterChronic: cleanValue(row[1]),
+          freshwaterAcute: cleanValue(row[2]),
+          saltwaterChronic: cleanValue(row[3]),
+          saltwaterAcute: cleanValue(row[4])
+        };
+      }, nextStep);
+    },
+
+    /*
+     * Table D-4e
+     */
+    function (nextStep) {
+      importData(data, 'tableD4e', 'TABLE D-4e. SUMMARY OF USEPA AND OTHER PUBLISHED AQUATIC HABITAT GOALS', 5, function (row) {
+        return {
+          chemical: cleanValue(row[0]),
+          freshwaterUSEPAChronic: cleanValue(row[1]),
+          freshwaterUSEPAAcute: cleanValue(row[2]),
+          freshwaterOtherChronic: cleanValue(row[3]),
+          freshwaterBasisChronic: cleanValue(row[4]),
+          freshwaterOtherAcute: cleanValue(row[5]),
+          freshwaterBasisAcute: cleanValue(row[6]),
+          marineUSEPAChronic: cleanValue(row[7]),
+          marineUSEPAAcute: cleanValue(row[8]),
+          marineOtherChronic: cleanValue(row[9]),
+          marineBasisChronic: cleanValue(row[10]),
+          marineOtherAcute: cleanValue(row[11]),
+          marineBasisAcute: cleanValue(row[12])
+        };
+      }, nextStep);
+    },
+
+    /*
+     * Table D-4f
+     */
+    function (nextStep) {
+      importData(data, 'tableD4f', 'TABLE D-4f. SURFACE WATER QUALITY STANDARDS FOR BIOACCUMULATION', 4, function (row) {
+        return {
+          chemical: cleanValue(row[0]),
+          selectedCriteria: cleanValue(row[1]),
+          basis: cleanValue(row[2]),
+          HIDOHWQS: cleanValue(row[3]),
+          USEPANWQC: cleanValue(row[4])
+        };
+      }, nextStep);
+    },
+
+    /*
+     * Table D-5
+     */
+    function (nextStep) {
+      importData(data, 'tableD5', 'TABLE D-5. CALIFORNIA AGRICULTURAL', 4, function (row) {
+        return {
+          chemical: cleanValue(row[0]),
+          agriculturalWaterQualityGoals: cleanValue(row[1])
         };
       }, nextStep);
     },
