9a721ff85beace42791dd20921c23e4d97c222b7
Justin Zaun <justin.zaun@gmail.com>
2017-09-16 13:59:44 -1000

    Add chemical notes to the report

--- src/client/components/ChemicalReport.vue
+++ src/client/components/ChemicalReport.vue
@@ -1,7 +1,9 @@
 <template lang="pug">
   .box.page-break
-    span.is-size-4.has-text-weight-light Chemical of concern:
+    div.is-size-4.has-text-weight-light Chemical of concern:
       span.has-text-weight-semibold &nbsp;{{ chemical.chemical }}
+    div.notes-title.is-size-5.has-text-weight-light Notes:
+    div.notes.is-size-6.has-text-weight-light {{ chemical.notes }}
     table.table.is-striped.is-fullwidth
       tbody(v-for='eal in chemicalEals(chemical.chemical).eals')
         tr
@@ -46,6 +48,12 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+.notes-title
+  float left
+.notes
+  padding-top 4px
+  padding-left 4em
+
 @media print
   .box
     margin-top 15mm
